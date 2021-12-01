ec43e5751169e95740f684971bb1b96327f67fa9
Jose Naime <josep.naimeg@gmail.com>
2021-12-01 16:09:18 -0600

    release 1.0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import {Formik, Field, Form} from 'formik';
 
 function App() {
-  return (
-    <div>
+    return (
+        <div className="background">
+            <div>
+                <Formik
+                    initialValues={{email: '', password: ''}}
+                    validate={values => {
+                        const errors = {};
+                        if (!values.email) {
+                            errors.email = 'Field Required';
+                        } else if (
+                            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                        ) {
+                            errors.email = 'Invalid email address';
+                        }
 
-    </div>
-  );
+                        if (!values.password) {
+                            errors.password = 'Field Required';
+                        }
+                        return errors;
+                    }}
+                    onSubmit={(values, {setSubmitting}) => {
+                        setTimeout(() => {
+                            alert("Login successful!");
+                            setSubmitting(false);
+                        }, 400);
+                    }}
+                >
+                    {({
+                          values,
+                          errors,
+                          touched,
+                          handleChange,
+                          handleBlur,
+                          handleSubmit,
+                          isSubmitting,
+                      }) => (
+                        <Form onSubmit={handleSubmit}>
+                            <h1>Login</h1>
+                            <div>
+                                <label htmlFor="email">Email</label>
+                                <Field
+                                    id={"emailField"}
+                                    type="email"
+                                    name="email"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.email}
+                                />
+                                <aside id="emailError">{errors.email && touched.email && errors.email}</aside>
+                            </div>
+                            <div>
+                                <label htmlFor="password">Password</label>
+                                <Field
+                                    id={"pswField"}
+                                    type="password"
+                                    name="password"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.password}
+                                />
+                                <aside id="pswError">{errors.password && touched.password && errors.password}</aside>
+                            </div>
+                            <button id="submitBtn" type="submit" disabled={isSubmitting}>
+                                Submit
+                            </button>
+                        </Form>
+                    )}
+                </Formik>
+            </div>
+        </div>
+    );
 }
 
 export default App;
